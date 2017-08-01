35e5545e311f8e8e5f88d759570347d13f76333a
ayubov <alvi.ayubov@yandex.ru>
2017-08-01 15:10:32 +0300

    add practice

---
+++ 03-friend-or-foe.js
@@ -0,0 +1,11 @@
+// task description
+/*
+Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours!
+
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+*/
+
+// solution
+const friend = friends => friends.filter(n => n.length === 4);
