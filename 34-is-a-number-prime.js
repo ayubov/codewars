ea2614915cf8e88e5eb758c3d7c1c5252eb432a9
ayubov <alvi.ayubov@yandex.ru>
2017-09-11 16:31:13 +0300

    add practice

---
+++ 34-is-a-number-prime.js
@@ -0,0 +1,34 @@
+// task description
+/*
+Is Prime
+
+Define a function isPrime/is_prime() that takes one integer argument and returns true/True or false/False depending on if the integer is a prime.
+
+Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+Example
+
+isPrime(5)
+=> true
+Assumptions
+
+You can assume you will be given an integer input.
+You can not assume that the integer will be only positive. You may be given negative numbers as well (or 0).
+*/
+
+// solution
+const isPrime = (num) => {
+  if (num < 2) {
+  return false;
+  }
+  const iter = (num,acc) => {
+   if (acc === 1) {
+    return true;
+   }
+   if (num % acc === 0) {
+   return false;
+   }
+   return iter(num,acc-1);
+  }
+  return iter(num,num-1);
+}
